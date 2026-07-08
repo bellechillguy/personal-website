a3480a82a7b102c6b68f8bd96decbb0a1b3f03de
Nisrina Zakiyah <19624039@mahasiswa.itb.ac.id>
2026-07-08 10:53:09 +0700

    chore: add project configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tanstackStart(), react(), tailwindcss()],
+  plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    host: "::",
+    host: "127.0.0.1",
     port: 8080,
   },
 });
