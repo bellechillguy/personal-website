a3cf99fde94a5466724b16226800dc1d6b018c83
Nisrina Zakiyah <19624039@mahasiswa.itb.ac.id>
2026-07-22 12:33:08 +0700

    feat(branding): add custom site favicon

--- src/root.tsx
+++ src/root.tsx
@@ -12,6 +12,8 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="color-scheme" content="light dark" />
+        <link rel="icon" type="image/png" href="/favicon.png?v=1" />
+        <link rel="apple-touch-icon" href="/favicon.png?v=1" />
         <script dangerouslySetInnerHTML={themeBootScript} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
